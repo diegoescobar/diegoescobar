a62ed3792f4b7f7fd08baecc45db65079ce73457
DiegoEsco <ratcore@gmail.com>
2019-07-16 21:08:16 -0400

    Initial commit

---
+++ precache-manifest.f8f9145a5b3d575a6374ab17a6f82bc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5b108a24f7381bfa78880e7a30825c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63461b59be461ef606ba",
+    "url": "/static/css/main.f2e95690.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "63461b59be461ef606ba",
+    "url": "/static/js/main.3364d1ce.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
